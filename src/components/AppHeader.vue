767d4f2e398190a6e215284e1efc228b61215b8b
UnaLai <qqq323618@gmail.com>
2020-06-17 16:00:03 +0800

    [feat]:add AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      loggedIn: false
+    };
+  }
+};
+</script>
